4219e57bcf921f10a23c6bb54d6fb25a276d5278
JackMallett <jacktheboss03@gmail.com>
2022-03-02 11:02:50 -0600

    added cube method

--- cube.py
+++ cube.py
@@ -1,13 +1,13 @@
 import collections
+from itertools import permutations
+from itertools import product
 
-
-
-class cube:
+class block:
     def __init__(self, x,  y, z):
         self.x = x
         self.y = y
         self.z = z
-        self.faces = ["W", "R", "G", "Y", "O" , "B",]
+        self.faces = ["W", "R", "G", "Y", "O" , "B"]
         self.FB = ["W", "Y"]
         self.RL = ["G", "B"]
         self.DU = ["R", "O"]
@@ -50,12 +50,78 @@ class cube:
         
     def arr(self):
         print(self.faces)
-        
-            
-Cube = cube(0, 0, 0)
-Cube.show()
-Cube.arr()
 
-Cube.rotate("r")
-Cube.show()
-Cube.arr()
+    def getf(self, face):
+        return self.faces[face]
+
+    def coords(self):
+        return self.x, self.y, self.z
+        
+class cube:
+    def __init__(self):
+        self.blocks = []
+        for combo in product(range(3), repeat=3):
+            temp = block(combo[0], combo[1], combo[2])
+            self.blocks.append(temp)
+
+    def getFace(self, face):
+        if face == "r":
+            n = 0
+            a = 2
+        elif face == "l":
+            n = 0
+            a = 0
+        elif face == "u":
+            n = 1
+            a = 2
+        elif face == "d":
+            n = 1
+            a = 0
+        elif face == "f":
+            n = 2
+            a = 0
+        elif face == "b":
+            n = 2
+            a = 2
+
+        temp = []
+
+        for x in self.blocks:
+            if x.coords()[n] == a:
+                temp.append(x)
+
+        return temp
+    
+    def turn(self, face):
+        temp = self.getFace(face)
+        for x in temp:
+            x.rotate(face)
+
+    def out(self):
+        for x in self.blocks:
+            print(x.faces)
+                
+    def getCube(self, a, b, c):
+        for i in self.blocks:
+            if i.x == a and i.y == b and i.z == c:
+                return i
+
+    #def display(self):
+
+
+
+
+
+
+
+Cube = cube()
+temp = (Cube.getCube(2,2,0))
+print(temp.faces)
+#Cube.turn("r")
+#Cube.out()
+# Cube.show()
+# Cube.arr()
+
+# Cube.rotate("r")
+# Cube.show()
+# Cube.arr()
